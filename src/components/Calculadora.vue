eb46ef4d942cc510fafd96e006f0c94ebb6e1d97
jcox <jimy.coxr@ug.edu.ec>
2021-07-23 11:38:48 -0500

    Inicio del proyecto

---
+++ src/components/Calculadora.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>This is a calculator</h1>
+        <span>{{ resultado }}</span>
+
+        <v-row
+            align="center"
+            justify="space-around"
+        >
+            <v-btn
+            depressed
+            color="primary"
+            >
+            Primary
+            </v-btn>
+        </v-row>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"calculadora",
+    props:{
+            resultado: String
+        }
+}
+</script>
