c108c3f58d138f4875c8af24d925c559b6d11706
jcampoverde <j.campoverded@gmail.com>
2021-09-07 18:16:29 -0500

    cambios

--- src/helpers/obtenerClaveValorPostman.js
+++ src/helpers/obtenerClaveValorPostman.js
@@ -1,15 +1,9 @@
 export const obtenerClaveValorPostman = (arreglo) =>{
     console.log(arreglo);
-    /* let listaClaveValor = {};
-    let objetoClaveValor = {}; */
     let value = new URLSearchParams();
     arreglo.forEach(element => {
         value.append(element.key, decodeURIComponent(element.value));
     })
-    
-    /* const params = new URLSearchParams(obtenerClaveValorPostmanHeader(arreglo)).toString(); */
-    
-    
     console.log(value);
     return value;
 }
